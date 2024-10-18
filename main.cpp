38669aa7ef011f6bc7508d0280b221b75e27f91b
mandel <mandel.cohen@student.forsbergsskola.se>
2024-10-18 12:57:07 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <SDL.h>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    } else {
+        std::cout << "SDL initialized successfully!" << std::endl;
+    }
+
+    SDL_Window *win = SDL_CreateWindow("Hello SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    if (win == nullptr) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    } else {
+        std::cout << "Window created successfully!" << std::endl;
+    }
+
+    SDL_Event e;
+    bool quit = false;
+    while (!quit) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+        SDL_Delay(16); // Delay to cap the frame rate
+    }
+
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+    return 0;
+}
