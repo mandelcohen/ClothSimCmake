9ccf6a6564bbddc6633d973bc3c4e6b029da05dd
mandel <mandel.cohen@student.forsbergsskola.se>
2024-11-06 10:06:38 +0100

    Update main loop

--- main.cpp
+++ main.cpp
@@ -3,11 +3,18 @@
 
 int main(int argc, char* argv[]) {
     SDLWindow window(640, 480);
-    Cloth cloth(10, 10, 20.0f);
+    Cloth cloth(10, 10, 20.0f, 0.99f);
+
+    const float deltaTime = 0.033f;
 
     bool running = true;
+
     while (running) {
         running = window.handleEvents();
+
+        cloth.applyForces(deltaTime);
+        cloth.integrateMotion(deltaTime);
+        cloth.enforceConstraints(3);
 
         window.Render(cloth);
 
